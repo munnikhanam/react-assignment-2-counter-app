71eba31d31c2baf2013a4effd60dd25bd6b99ade
munnikhanam <munni3khanom@gmail.com>
2023-06-01 19:46:31 +0600

    All code

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrementCounter = () => {
+    console.log('clicked');
+    setCount(count + 1);
+  };
+  const handleDecrementCounter = () => {
+    setCount(count - 1);
+  };
+  const handleCounter = () => {
+    setCount(0);
+  };
+
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count : {count}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button
+            className="btn card__btn"
+            onClick={handleIncrementCounter}
+            disabled={count === 5 ? true : false}>
+            +
+          </button>
+          <button
+            className="btn card__btn"
+            onClick={handleDecrementCounter}
+            disabled={count === 0 ? true : false}>
+            -
+          </button>
+          <button className="btn card__btn" onClick={handleCounter}>
+            0
+          </button>
         </div>
       </div>
     </div>
